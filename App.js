9fea8ce8b84d16be8c65b45005a4edb6dce7c1d0
sparklinstar <souradip1000@gmail.com>
2023-12-28 07:42:38 +0530

    changed directory, added screens folder, played around with flex, created a Home Screen

--- App.js
+++ App.js
@@ -1,61 +1,7 @@
-import {
-  Alert,
-  Button,
-  Platform,
-  StyleSheet,
-  Text,
-  View,
-  StatusBar,
-  Dimensions,
-} from "react-native";
+import React from "react";
+import { View } from "react-native";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
-  //console.log(Dimensions.get("screen")); //Not get updated when orientation changes
-  return (
-    <View
-      style={{
-        backgroundColor: "white",
-        flex: 1,
-        flexDirection: "row",
-        justifyContent: "center",
-        alignItems: "center",
-        alignContent: "center",
-        flexWrap: "wrap",
-      }}
-    >
-      <View
-        style={{
-          backgroundColor: "lightgreen",
-          height: 100,
-          width: 100,
-        }}
-      ></View>
-      <View
-        style={{
-          backgroundColor: "blue",
-          height: 100,
-          width: 100,
-          right: 120,
-          bottom: 120,
-        }}
-      ></View>
-      <View
-        style={{ backgroundColor: "orange", height: 100, width: 100 }}
-      ></View>
-      <View
-        style={{ backgroundColor: "black", height: 100, width: 100 }}
-      ></View>
-      <View style={{ backgroundColor: "grey", height: 100, width: 100 }}></View>
-    </View>
-  );
+  return <WelcomeScreen />;
 }
-
-const containerStyle = { backgroundColor: "orange" };
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "red",
-
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-  },
-});
